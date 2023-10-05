8c70e25d20db96028ca48293ed59220d3ec9f592
Jordncodes <jordanwalters681@google.com>
2023-10-05 10:34:16 +0100

    createTables writing first script

--- createTables.js
+++ createTables.js
@@ -1,2 +1,20 @@
 // Import the pg-promise library
 import pg from "pg";
+
+// Define the connection string for your ElephantSQL database
+const connectionString = process.env.DB_CONNECTION_STRING;
+
+// Function to connect to the database
+const pool = new pg.Pool({
+    connectionString
+});
+
+
+// Send a query - SELECT * FROM books;
+const data = await pool.query("SELECT * FROM authors;");
+const rows = data.rows;
+console.log(rows);
+
+
+//close the connection
+await pool.end();
